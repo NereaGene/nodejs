c2658f08b5fe93146b7ffd6b0b01f3b707538940
NereaGene <neramirez25@gmail.com>
2018-02-27 12:01:33 +0100

    ficheros

---
+++ lectura-ficheros/async.js
@@ -0,0 +1,36 @@
+/*
+    Primer ejemplo de código Asíncrono NodeJS en el servidor
+*/
+
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+var html="";
+
+var manejadoraServidor = function(req, res) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(html);
+        res.end();
+    };
+
+var manejadoraFichero = function(err, info){
+    html=info;
+    const server = http.createServer(manejadoraServidor);
+    server.listen(port, hostname, 
+        //aunque resulta un poco raro al principio, significa function () {
+        () => {
+            console.log(`Server running at http://${hostname}:${port}/`);
+        }
+    );
+};
+
+fs.readFile("./index.html",manejadoraFichero);
+
+
+
+
+
